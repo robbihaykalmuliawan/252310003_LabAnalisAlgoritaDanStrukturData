707666dd1926d2b4e1257819195d704311c5303b
Jaden <robbihaykalmuliawan@gmail.com>
2025-10-06 00:16:02 +0700

    Rapihkan FIle Pertemuan 2

---
+++ Rapihkan Pertemuan 2/Soal[1].cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int A,B,C,D,E,F;
+    cout << "Nomor 1 - Penjumlahan" ;
+    cout << "\nMasukkan 2 angka" ;
+    cout << "\nA = " ;
+    cin >> A;
+    cout << "B = " ;
+    cin >> B;
+    cout << "Hasil A + B = " << A + B << endl ;
+
+    cout << "\nNomor 2 - Pengurangan" ;
+    cout << "\nMasukkan 2 angka" ;
+    cout << "\nC = " ;
+    cin >> C;
+    cout << "D = " ;
+    cin >> D;
+    cout << "Hasil C - D  = " << C - D << endl ;
+
+    cout << "\nNomor 3 - Perkalian" ;
+    cout << "\nMasukkan 2 angka" ;
+    cout << "\nE = " ;
+    cin >> E;
+    cout << "F = " ;
+    cin >> F;
+    cout << "Hasil E * F  = " << E * F << endl ;
+
+    return 0;
+}
---
+++ Rapihkan Pertemuan 2/int.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main ()
+{
+	int x;
+	x = 14;
+	cout << "Nilai X adalah:" << x;
+	
+	return 0;
+}
+
