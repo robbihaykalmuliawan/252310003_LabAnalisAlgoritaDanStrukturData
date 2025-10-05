8e8f3a3483129482ed6228c008a700cdf21575ee
Jaden <robbihaykalmuliawan@gmail.com>
2025-10-05 23:52:32 +0700

    Revert " Rapihkan File Pertemuan 2"
    This reverts commit e4d1bde88839f0ece21c67f8f78f68fb95229e26.

---
+++ char.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	char y[30]; char z[35]; char x[40];
+	
+	cout << "Masukan NPM Anda: ";
+	cin >> x;
+	cout << "Masukan Nama Anda: ";
+	cin >> y;
+	cout << "Masukan Alamat Anda: ";
+	cin >> z;
+	
+	cout << "NPM Anda adalah: " << x;
+	cout << "\nNama Anda adalah: " << y;
+	cout << "\nAlamat Anda adalah: " << z;
+	
+}
