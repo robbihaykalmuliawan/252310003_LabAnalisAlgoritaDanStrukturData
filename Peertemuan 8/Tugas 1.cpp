083c7a48fc7adb1bd14b9bd77e54e5602a03b85f
Jaden <robbihaykalmuliawan@gmail.com>
2025-11-24 21:39:02 +0700

    Mengupload Tugas

---
+++ Peertemuan 8/Tugas 1.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main () {
+	
+	string kata = "FAKULTAS PARIWISATA DAN TEKNOLOGI INFORMASI";
+	cout << "Kalimat: " << kata << endl ;
+	cout << "Panjang Kalimat: " << kata.length();
+	
+}
---
+++ Peertemuan 8/Tugas 3.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main () {
+	
+	string kalimat1 = "Teknik";
+	string kalimat2 = "Informatika";
+	
+	string gabungan = kalimat1 + " "+ kalimat2;
+	cout << "Hasil Gabungan : " << gabungan;
+}
